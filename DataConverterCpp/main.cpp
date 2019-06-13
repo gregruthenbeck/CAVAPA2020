1c33a1f41d75267572a4172fd8f639938dc7080a
Greg Ruthenbeck <greg.ruthenbeck@gmail.com>
2019-06-13 11:00:20 +0300

    comments and logging

--- DataConverterCpp/main.cpp
+++ DataConverterCpp/main.cpp
@@ -22,6 +22,7 @@ struct AccDataReadMarker {
 	unsigned char day, month, year, hour, min, sec;
 };
 
+// ".dat" files contain time markers with weird format
 unsigned char hexToInt(unsigned char c) {
 	return (c / 16 * 10) + c % 16;
 }
@@ -96,13 +97,8 @@ public:
 std::ostream& operator<<(std::ostream& os, const CTime& t) {
 	SYSTEMTIME st;
 	t.GetAsSystemTime(st);
-	os << st;
-	return os;
-}
-
-std::ostream& operator<<(std::ostream& os, const SYSTEMTIME& st) {
 	os << st.wDay << "." << st.wMonth << "." << st.wYear << " " <<
-		  st.wHour << ":" << setfill('0') << setw(2) << st.wMinute << ":" << setfill('0') << setw(2) << st.wSecond;
+		st.wHour << ":" << setfill('0') << setw(2) << st.wMinute << ":" << setfill('0') << setw(2) << st.wSecond;
 	return os;
 }
 
@@ -113,6 +109,15 @@ std::ostream& operator<<(std::ostream& os, const CTimeSpan& dur) {
 	return os;
 }
 
+/*
+	There are 16,419,847 samples in the 1st accelerometer file. At 100Hz, this represents
+	16419847/100/60/60 = 45.6107 hrs. The file creation time (acc1.dat) is ‎Monday, ‎27 ‎May ‎2019, ‏‎5:33:00 PM.
+	The activities of interest occurred on Sunday afternoon from about 2:30pm onwards.
+	That’s 27hrs33mins earlier than the file creation time. 45.6 - 27.5 = 18.1hrs from the
+	start of the recordings is the start of the interesting data. 18.1 * 100*60*60=6.516E6 samples
+	can be skipped at the beginning of hr.dat, and not more than 2hrs of the subsequent data is of
+	interest (2*100*60*60=720,000 samples for the 2hrs at 100Hz).
+*/
 CTimeSpan BytesToTime(const unsigned long bytes, const unsigned bytesPerSecond) {
 	unsigned long t = bytes / bytesPerSecond; // 100Hz, 6-bytes per sample (x/y/z*ushort)
 	const unsigned secs = t % 60;
@@ -147,35 +152,59 @@ const CTime fileEndTimes[8] = {
 		{2019,05,27,18,00,28},
 		{2019,05,27,18,04,06} };
 
+template < class T >
+std::ostream& operator << (std::ostream& os, const std::vector<T>& v) {
+	os << "[";
+	for (typename std::vector<T>::const_iterator ii = v.begin(); ii != v.end(); ++ii) {
+		os << " " << *ii;
+	}
+	os << "]";
+	return os;
+}
 
 int main() {
 	const string inFolder = "../data_in";
 	const string inFileExt = ".dat";
 	const vector<string> inFilenames{ "acc1", "acc2", "acc3", "acc4", "acc5", "acc6", "acc7", "acc8" };
 	const string outFilepath = "../data_out/acc-all.csv";
+	const int samplingRateHz = 100;
 	const int averagingWindowLen = 3000;
 	const int averagingWindowLen2 = 1000;
+	CTime roiStartTime(2019, 5, 26, 15, 0, 0); // The experiment started at about 2:30pm on Sunday 26/5/2019
+	const unsigned long roiLen = 360000; // samples
+	const unsigned saveInterval = 100; // write every 'n' samples (skip the rest)
+
+	cout << "Input folder:\t\"" << inFolder << "\"" << endl;
+	cout << "Input file extension:\t\"" << inFileExt << "\"" << endl;
+	cout << "Input file names:\t" << inFilenames << endl;
+	cout << "Output file-path:\t\"" << outFilepath << "\"" << endl;
+	cout << "AveWinLen:\t" << averagingWindowLen << endl;
+	cout << "AveWinLen2:\t" << averagingWindowLen2 << endl;
+	cout << "ROI start time:\t" << roiStartTime << endl;
+	cout << "ROILen:\t" << roiLen << " samples (" << (roiLen / (samplingRateHz * 60 * 60)) << "min)" << endl;
+	cout << "SaveInterval:\t" << saveInterval << " (" << (saveInterval / samplingRateHz) << "Hz)" << endl;
+
+	if ((saveInterval / samplingRateHz) != 1) {
+		cout << "WARNING: CSV data is written assuming that output rows represent 1Hz" << endl;
+	}
 
 	vector<string> inFilepaths;
 	for (auto& fname : inFilenames)
 		inFilepaths.push_back(inFolder + "/" + fname + inFileExt);
 
 	// Estimate the start times from the number of samples and the file-creation time
-	vector<unsigned int> fileSizesBytes;
-	for (auto& fp : inFilepaths) {
-		fileSizesBytes.push_back(file_size(fp.c_str()));
-	}
+	vector<unsigned long> fileSizesBytes;
+	for (auto& fp : inFilepaths) 
+		fileSizesBytes.push_back((unsigned long)file_size(fp.c_str()));
 
 	vector<CTimeSpan> fileDurations;
-	for (auto& fs : fileSizesBytes) {
-		fileDurations.push_back(BytesToTime(fs, 100 * 6)); // 100Hz, 6-bytes per sample (x/y/z*ushort)
-	}
-
+	for (auto& fs : fileSizesBytes) 
+		fileDurations.push_back(BytesToTime(fs, samplingRateHz * 6)); // 100Hz, 6-bytes per sample (x/y/z*ushort)
+	
 	vector<CTime> fileStartTimes;
-	for (int i = 0; i != 8; ++i) {
+	for (int i = 0; i != 8; ++i) 
 		fileStartTimes.push_back(fileEndTimes[i] - fileDurations[i]);
-	}
-
+	
 	cout << "ID, Start, Duration, End" << endl;
 	for (int i = 0; i != 8; ++i) {
 		cout << (i + 1) << ", Start "    << fileStartTimes[i] << 
@@ -183,13 +212,12 @@ int main() {
 						   ", End "      << fileEndTimes[i] << endl;
 	}
 
-	CTime startTime(2019, 5, 26, 15, 0, 0); // The experiment started at about 2:30pm on Sunday 26/5/2019
 	vector<unsigned long> startSampleIds;
 	for (int i = 0; i != 8; ++i) {
-		//CTimeSpan delta = startTime - fileStartTimes[i]; // dist from start
-		//startSampleIds.push_back(100 * delta.GetTotalSeconds());
-		CTimeSpan delta = fileEndTimes[i] - startTime; // dist from end
-		startSampleIds.push_back(100 * (fileDurations[i].GetTotalSeconds() - delta.GetTotalSeconds()));
+		CTimeSpan delta = roiStartTime - fileStartTimes[i]; // dist from start
+		startSampleIds.push_back(100 * delta.GetTotalSeconds());
+		//CTimeSpan delta = fileEndTimes[i] - startTime; // dist from end
+		//startSampleIds.push_back(100 * (unsigned long)(fileDurations[i].GetTotalSeconds() - delta.GetTotalSeconds()));
 	}
 
 	vector<std::ifstream> inFiles;
@@ -208,6 +236,7 @@ int main() {
 	}
 
 	vector<vector<double> > filesData;
+	vector<unsigned long> startedAtSampleId(8, 0L);
 
 	int fileCount = 0;
 	for (auto& inFile : inFiles) {
@@ -219,20 +248,10 @@ int main() {
 		windowerLong.SetWindowLength(averagingWindowLen2);
 		AccelData data;
 		vector<double> fileData;
-		/*
-		There are 16,419,847 samples in the accelerometer file. At 100Hz, this represents
-		16419847/100/60/60 = 45.6107 hrs. The file creation time (acc.dat) is ‎Monday, ‎27 ‎May ‎2019, ‏‎5:33:00 PM.
-		The activities of interest occurred on Sunday afternoon from about 2:30pm onwards.
-		That’s 27hrs33mins earlier than the file creation time. 45.6 - 27.5 = 18.1hrs from the
-		start of the recordings is the start of the interesting data. 18.1 * 100*60*60=6.516E6 samples
-		can be skipped at the beginning of hr.dat, and not more than 2hrs of the subsequent data is of
-		interest (2*100*60*60=720,000 samples for the 2hrs at 100Hz).
-		*/
+
 		unsigned long start = numeric_limits<unsigned long>::max();// startSampleIds[fileCount - 1];// 6.516E6L;// -(30 * 60 * 100); // start 30mins earlier (30*60*100)
-		unsigned long duration = 360000;
 		unsigned long count = 0L;
-		unsigned long writtenCount = 0L;
-		unsigned saveInterval = 100; // write every 'n' samples (skip the rest)
+		unsigned long roiCount = 0L;
 		vector<CTime> markers;
 		while (!inFile.eof() && inFile.good()) {
 			try {
@@ -251,21 +270,22 @@ int main() {
 				}
 				inFile.read((char*)& data, sizeof(AccelData));
 				++count;
-				auto delta = startTime - markers.back();
+				auto delta = roiStartTime - markers.back();
 				if (delta.GetTotalSeconds() < 1) {
 					start = count;
+					startedAtSampleId[fileCount - 1] = start;
 				}
 				if (count < start) {
 					continue;
 				}
-				++writtenCount;
-				if (writtenCount > duration) {
+				++roiCount;
+				if (roiCount > roiLen) {
 					break;
 				}
 				const double len = Length(data);
 				const double ave = windower.AddGetAverage(len);
 				const double longAve = windowerLong.AddGetAverage(ave);
-				if (writtenCount % saveInterval == 0) {
+				if (roiCount % saveInterval == 0) {
 					fileData.push_back(abs(ave - longAve));
 				}
 			}
@@ -282,7 +302,7 @@ int main() {
 	}
 	const char sep = ',';
 	for (size_t i = 0; i < minSize; ++i) {
-		outFile << i << sep << ((double)i / 86400.0) << sep; // second column is helper for formatting seconds as time in excel
+		outFile << i << sep << ((double)i / 86400.0) << sep; // 2nd column is helper for formatting seconds as time in excel
 		for (auto& d : filesData) {
 			outFile << d[i] << sep;
 		}
@@ -290,5 +310,14 @@ int main() {
 	}
 	outFile.flush();
 	outFile.close();
+
+	// "startSampleIds" is calculated from the file creation date-time and the file size
+	// "startedAtSampleId" uses the time markers stored in the .dat file at 512byte intervals
+	for (int i = 0; i != 8; ++i) {
+		auto a = startSampleIds[i];
+		auto b = startedAtSampleId[i];
+		cout << a << ", " << b << ", " << abs((long)a - (long)b) << endl;
+	}
+
 	cout << "done" << endl;
 }
